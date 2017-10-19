47a101e04d5c86a87617f005b46392b479e9f8de
Jerry Wisdom <krabique48@gmail.com>
2017-10-19 19:14:58 +0000

    All hail rubocop!

--- food.rb
+++ food.rb
@@ -1,13 +1,14 @@
 require 'io/console'
 require 'time'
 
+# Monkey-patching Time class for some handy methods
 class Time
   def add_hours(h)
     self + (3600 * h)
   end
-  
+
   def hours_and_minutes
-    self.strftime('%H:%M')
+    strftime('%H:%M')
   end
 end
 
@@ -18,17 +19,16 @@ end
 
 time = Time.strptime(prompt('Breakfast:  '), '%H:%M')
 
-breakfast = time
+# breakfast = time
 elevenses = time.add_hours(2.5)
 dinner = elevenses.add_hours(2.5)
 tea = dinner.add_hours(3)
 supper = tea.add_hours(3)
 
-print 'Elevenses:  ' + elevenses.hours_and_minutes   + "\n" +
-      'Dinner:     ' + dinner.hours_and_minutes      + "\n" +
-      'Tea:        ' + tea.hours_and_minutes         + "\n" +
+print 'Elevenses:  ' + elevenses.hours_and_minutes   + "\n" \
+      'Dinner:     ' + dinner.hours_and_minutes      + "\n" \
+      'Tea:        ' + tea.hours_and_minutes         + "\n" \
       'Supper:     ' + supper.hours_and_minutes      + "\n"
-
 
 print "\n---- press any key to exit this shit ----\n"
 STDIN.getch
